57ac07246384832eadfbc454ccd011dd3b3ddc98
Scott A <scottandermann@gmail.com>
2022-07-26 15:02:39 -0400

    added comments

--- src/SpinTimer/SpinTimer.js
+++ src/SpinTimer/SpinTimer.js
@@ -11,18 +11,19 @@ function SpinTimer() {
     const [isActive, setIsActive] = useState(false);
     const [animation, setAnimation] = useState(0);
 
+
     function timerAnimation(time) {
+        // recursive function used to get accurate timing from initial click
         if (startTime === undefined) {
-            // setStartTime(time);
+            // set starting time - this is not set as a state of the page though
             startTime = time;
         }
-        // console.log(startTime);
         const elapsed = time - startTime;
 
         if (prevTime !== time && !done) {
             const count = Math.min(elapsed, 5000);
             setTimer(count);
-            if (count === 500) {
+            if (count === 5000) {
                 done = true;
                 startTime = undefined;
             }
@@ -42,12 +43,13 @@ function SpinTimer() {
             window.requestAnimationFrame(timerAnimation)
         }
         if (!isActive) {
+            // reset all attributes so that page can be run again when isActive flag is changed to false.
             setTimer(0);
             startTime = undefined;
             done = false;
         }
-
-    }, [timer, isActive]);
+        
+    }, [timer, isActive]); //trigger useEffect loop on timer and isActive state changes
 
 
     const handleClick = () => {
